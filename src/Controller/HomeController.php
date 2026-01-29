0018099e377421e9bae9fe000eebabf32d8f6921
Emilie Rollandin <emilie.rollandin@gmail.com>
2026-01-29 22:10:36 +0000

    creazione prenotazione

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Form\PrenotazioneType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_prenotazione')]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $elemento = null;
+        $form = $this->createForm(PrenotazioneType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $elemento = $form->getData();
+                       
+            dd($elemento);
+
+            $em->persist($elemento);
+            $em->flush();          
+
+            return $this->redirectToRoute('app_prenotazione');
+        }
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
+
 }
---
+++ src/Form/PrenotazioneType.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Albergo;
+use App\Entity\Porteur;
+use App\Entity\Prenotazione;
+use App\Entity\Tariffa;
+use App\Entity\TipologiaOspitalita;
+use App\Entity\TipologiaSistemazione;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class PrenotazioneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fkPorteur', EntityType::class, [
+                'class' => Porteur::class,
+                'choice_label' => 'Descrizione',
+                'label' => 'Porteur',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-select',
+                ]
+            ])
+            ->add('Cliente', TextType::class, [
+                'label' => 'Cliente', 
+                'label_attr' => ['class' => 'form-label'],
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Inserire il nome del cliente'
+                ]
+            ])
+            ->add('Dal', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Dal', 
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])            
+            ->add('Al', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Al', 
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])      
+            ->add('PaxAdulti', IntegerType::class, [
+                'label' => 'Pax adulti', 
+                'required' => true,
+                'data' => 1,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('PaxBambini', IntegerType::class, [
+                'label' => 'Pax 5-14 anni', 
+                'required' => true,
+                'data' => 0,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('PaxAdolescenti', IntegerType::class, [
+                'label' => 'Pax >15 anni', 
+                'required' => true,
+                'data' => 0,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('fkTipologiaOspitalita', EntityType::class, [
+                'class' => TipologiaOspitalita::class,
+                'choice_label' => 'Descrizione',
+                'label' => 'Tipologia ospitalità',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-select',
+                ]
+            ])
+            ->add('fkAlbergo', EntityType::class, [
+                'class' => Albergo::class,
+                'choice_label' => 'Descrizione',
+                'label' => 'Albergo',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-select',
+                ]
+            ])
+            ->add('fkTipologiaSistemazione', EntityType::class, [
+                'class' => TipologiaSistemazione::class,
+                'choice_label' => 'Descrizione',
+                'label' => 'Tipologia sistemazione',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-select',
+                ]
+            ])
+            ->add('fkTariffa', EntityType::class, [
+                'class' => Tariffa::class,
+                'choice_label' => 'Descrizione',
+                'label' => 'Tariffa',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-select',
+                ]
+            ])
+            ->add('Note', TextareaType::class, [
+                'label' => 'Note', 
+                'label_attr' => ['class' => 'form-label'],
+                'required' => false,
+                'data' => "",
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Prenotazione::class,
+        ]);
+    }
+}
